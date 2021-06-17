831477513e826adf24a4570af742978c3bbfc29e
Andrew Pham <andrewpham92@gmail.com>
2021-06-17 10:07:14 -0400

    Added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,23 +12,28 @@ public class Main {
 
         String notUsed = "abcdefghijklmnopqrstuvwxyz";
 
-        String[] words = new String[] { "computer", "mouse", "screen", "display", "language" };
 
+        // Create an array of words
+        String[] words = new String[] { "dog", "cat", "bird", "turtle", "fish" };
+
+        // Generate a word by accessing a random given index
         String randomWord = words[(int) (Math.random() * words.length)];
 
+        System.out.println("The category is type of animals.");
         System.out.println("The word has " + randomWord.length() + " letters.");
 
+        // Create a new char array by assigning each char given by the length of the given randomWord.
         char[] letters = new char[randomWord.length()];
 
 
-
+        // Starting lives
         int lives = 3;
 
         Scanner scanner = new Scanner(System.in);
 
         while (lives > 0) {
 
-
+            // Loop through the letters array and reassign it a "."
             for (int i = 0; i < letters.length; i++) {
                 letters[i] = '.';
             }
@@ -36,6 +41,7 @@ public class Main {
 
             System.out.print("Lives: ");
 
+            // Loop through the lives count and print out the lives
             for (int i = 0; i < lives; i++) {
                 System.out.print("O");
             }
@@ -46,14 +52,23 @@ public class Main {
 
             String input = scanner.nextLine();
 
+            // Store the input value from position 0
             char letter = input.charAt(0);
+
 
             boolean isGuessCorrect = false;
 
+
             for (int i = 0; i < randomWord.length(); i++) {
+                // Loop - Create a new variable and get the value of the given randomWord
                 char l = randomWord.charAt(i);
 
+
+                // Check if the user input stored in "letter" equals to any of the variable "l"
                 if (l == letter) {
+                    // Reassign the letters array by the matching index to the correct char
+                    // Ex: Original letters[] ==> {".",".","."}
+                    // If "c" was correct then the new letters[] ==> {"c",".","."}
                     letters[i] = l;
                     System.out.println("Guess is correct!");
                     isGuessCorrect = true;
@@ -69,6 +84,8 @@ public class Main {
 
             System.out.print("Word: ");
 
+
+            // check to see if the game is finished if the letters array still have a "."
             for (int i = 0; i < letters.length; i++) {
                 if (letters[i] == '.') {
                     isGameFinished = false;
